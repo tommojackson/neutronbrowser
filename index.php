c0a960474cfc3f27d98e249b0f3726c963c943ef
Tom Jackson <tom.jackson@justdevelop.it>
2017-05-24 07:23:33 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+
+	<head>
+
+		<title>NeutronBrowser - The Safest Browser Around</title>
+		<link rel="stylesheet" href="/style.css" type="text/css">
+		<meta name="viewport" content="width=device-width">
+		<link rel="shortcut icon" href="/images/favicon.ico">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="typed.js"></script>
+		<script>
+  			document.addEventListener('DOMContentLoaded', function(){
+      			Typed.new('.h1Typed', {
+        			strings: ["Safe", "Secure", "Protected", "Defended", "Guarded", "Shielded", "Immune", "Impregnable", "Impervious", "Inviolable", "Invulnerable", "Safeguarded", "Unthreatened", "Insulated", "NeutronBrowser"],
+        			typeSpeed: 100
+      			});
+  			});
+		</script>
+
+	</head>
+
+	<body>
+
+		<div class="topContent">
+
+			<div class="header">
+			
+				<div class="logoBox">
+				
+					<img class="nblogo" src="https://placeholdit.imgix.net/~text?txtsize=30&txt=NB+Logo&w=250&h=60&txttrack=0" >
+
+				</div>
+
+				<div class="navigationBox">
+
+					<ul class="navigation">
+
+						<li><a href="/contact">Contact</a></li>
+						<li><a href="/download">Download</a></li>
+
+					</ul>
+
+				</div>
+
+			</div>
+
+			<div class="h1Box">
+
+				<h1>This Browser is <span class="h1Typed"></span></h1>
+
+			</div>
+
+			<div class="siteBox">
+
+				<img class="siteimg" src="https://placeholdit.imgix.net/~text?txtsize=56&txt=Browser+On+Laptop&w=800&h=441&txttrack=0">
+
+			</div>
+
+		</div>
+
+		<div class="middleContent">
+
+			<div class="h2Box" style="margin-bottom: 200px;">
+
+				<h2>H2 Placeholder</h2>
+				<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet ligula ac magna malesuada dignissim.</span>
+
+			</div>
+
+		</div>
+
+	</body>
+
+</html>
