33f0a4831ab2fd0e176a610445ff6eb7de3b13e9
tomjackson <tom.jackson@justdevelop.it>
2017-05-25 19:01:41 +0100

    changed names of images

---
+++ lp.php
@@ -0,0 +1,56 @@
+<html>
+
+    <head>
+
+        <title>NeutronBrowser - The Safest Browser Around</title>
+        <link rel="stylesheet" href="/style.css" type="text/css">
+        <meta name="viewport" content="width=device-width">
+        <link rel="shortcut icon" href="/images/favicon.ico">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    </head>
+
+    <body>
+
+        <div class="lpHeader">
+
+            <div style="float: left;">
+
+                <span class="lpSiteName">Incognito Browser</span>
+
+            </div>
+
+            <div style="float: right;">
+
+                <span class="lpSiteDesc">The Most Secure Browser Around.</span>
+
+            </div>
+
+        </div>
+
+        <div class="lpMiddle">
+
+            <span class="lpCenterTitle">Your Download Is Ready</span>
+
+            <div class="lpDownloadBox">
+
+                <div class="col-left">
+
+                    steps
+
+                </div>
+
+                <div class="col-right">
+
+                    download button and info
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
