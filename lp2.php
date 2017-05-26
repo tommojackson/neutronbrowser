0d9ec36708b767742b64691dd87e5dc5d9736016
tomjackson <tom.jackson@justdevelop.it>
2017-05-26 14:55:59 +0100

    built lp2

--- lp2.php
+++ lp2.php
@@ -11,17 +11,74 @@
 
     </head>
 
-    <body>
+    <body class="lp2Body">
 
-        <div class="lp3header">
+        <div class="lp2Header">
 
-
+            <span style="float: left">Incognito Browser</span>
+            <span style="margin: 0 auto">Instant Browser Security</span>
 
         </div>
 
-        <div class="lp3middle">
+        <div class="lp2TopText">
+
+            <h2><span>ALL-IN-ONE</span> SECURE BROWSER FOR YOUR PC</h2>
+
+        </div>
+
+        <div class="lp2Download">
+
+            <div class="lp2ColLeft">
+
+                <div class="lp2SysInfo">
+
+                    <img src="/images/windows.png">
+
+                    <div class="lp2SysInfoText">
+
+                        <span style="font-weight: bold">System Information:</span>
+                        <span>You are currently running <?php echo "Windows 7"  ?></span>
+                        <span>Incognito Browser is compatible with your operating system</span>
+
+                    </div>
+
+                </div>
+
+                <div class="lp2DownloadBox">
+
+                    <div class="lp2Button">
+
+                        <div class="lp2ButtonText">
+
+                            <img src="/images/download.gif">
+
+                            START DOWNLOAD
+
+                        </div>
+
+                    </div>
+
+                    <div class="lp2DownloadInfo">
+
+                        <span><strong>Download Time:</strong> Around 20 seconds</span>
+                        <span><strong>Manufacturer:</strong> Incognito Browser</span>
+                        <span><strong>Designed For:</strong> Windows</span>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="lp2ColRight">
+
+                <div class="lp2Review">
 
 
+
+                </div>
+
+            </div>
 
         </div>
 
